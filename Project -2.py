191715214fd4d2436e100819bb4665eb9c13f8e7
kunal singh parmar <kunalsinghparmar13@gmail.com>
2026-09-15 21:14:41 +0530

    modified the code

--- Project -2.py
+++ Project -2.py
@@ -30,7 +30,8 @@ response = {
     "python": "Python is powerful — it can do AI, Automation, Web Dev and  much more!", 
     "sad": "Don't worry! Even code breaks sometimes, but it always runs again ", 
     "happy": "That's great to hear! Keep that positive energy going ", 
-    "bye": "Goodbye! Keep learning and keep smiling "
+    "bye": "Goodbye! Keep learning and keep smiling",
+    "what is your name?":"My name is PyGoo "
 }
 
 #5. Method to get response from ChatBot
